ff04b0275dd504d62c7b251b52eb2cfa19aec496
user <csscience@gnmail.com>
2020-11-27 15:02:25 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ const PORT = process.env.PORT || 3000
 //assets
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home');
-
-})
-
 //set template engine
 
 app.use(expressLayout)
@@ -25,6 +20,22 @@ app.set('views',path.join(__dirname,'/resources/views'))
 
 //express ko btana hai konsa template engine use karenge
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home');
+
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+
+})
+app.get('/login',(req,res) =>{
+    res.render('auth/login')
+
+});
+app.get('/register',(req,res) =>{
+    res.render('auth/register')
+});
 
 app.listen(3000, () => {
     console.log('listening port no 3000')
